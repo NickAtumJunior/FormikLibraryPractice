fbb4ae77be80a83b68e03d3df50871dfa50bc0bd
jaojaii <70026678+jaojaii@users.noreply.github.com>
2022-05-26 18:43:37 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,86 +1,118 @@
 import './App.css';
-import { * } from 'formik';
+import {useFormik} from 'formik';
+import * as Yup from "yup";
+import { Button, ButtonGroup, Dialog, DialogContent, Grid, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
 function App() {
+const [openDialog,setOpenDialog] = useState(false)
+
+const handleCloseDialog = () =>{
+  setOpenDialog(false)
+}
+
+const  openNow = () =>{
+  setOpenDialog(true)
+}
+ const formik = useFormik({
+
+       initialValues:{
+
+         username:"",
+         email: "",
+         passwordone: "",
+         passwordtwo: ""
+
+       },
+       validationSchema:Yup.object({
+        username: Yup.string().min(12,"Must be 12 or more characters").required("Required"),
+        email:Yup.string().email("Invalid email","^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$").required("Required"),
+        passwordone: Yup.string().required('Password is required'),
+        passwordtwo: Yup.string()
+           .oneOf([Yup.ref('passwordone'), null], 'Passwords must match'),
+        
+       }),
+       onSubmit: () =>{
+        console.log(formik.values)
+        
+       },
+       
+       
+
+ })
 
 
-  const formik = useFormik({
-          initialValues:{
-            username:"",
-            email:"",
-            passwordone: "",
-            passwordtwo: ""
-          },
-          validationSchema: Yup.object().shape({
-             username: Yup.string()
-             .max()
-          }),
-          onSubmit: (values) =>{
-                console.log(values)
-          },
-          onReset: (values) =>{
-             values.username = ""
-             values.email = ""
-             values.passwordone = ""
-             values.passwordtwo = ""
-          },
-           
-          
-  })
-  
   return (
     <form className="App" onSubmit={formik.handleSubmit}>
-      <h1>Form Using Formik and Yup</h1>
-        <div className='main'>
-
-           <div className='textfield'>
-             <label>Username: </label>
-               <input
-                type='text'
-                id='username'
-                name='username'
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                placeHolder='Enter Your Username'
+     
+        <Grid container display='flex' flexDirection='column' >
+          <center> <h1>Form Using Formik and Yup</h1></center>
+          <Stack spacing={3} alignItems='center'>
+           <Grid>
+               <TextField
+                 id='outlined-basic'
+                 label='Username'
+                 variant='outlined'
+                 name='username'
+                 value={formik.values.username}
+                 onChange={formik.handleChange}
                />
-           </div>
-
-           <div className='textfield'>
-             <label>Email: </label>
-               <input
-                type='email'
-                id='email'
+               {formik.errors.username ? <p className='errors'>{formik.errors.username}</p> : null}
+           </Grid>
+         
+           <Grid item>
+               <TextField
+                id='outlined-basic'
+                 label='Email'
+                 variant='outlined'
                 name='email'
                 value={formik.values.email}
                 onChange={formik.handleChange}
-                placeHolder='Enter Your Email-id'
                />
-           </div>
+               {formik.errors.email ? <p className='errors'>{formik.errors.email}</p> : null}
+           </Grid>
           
-           <div className='textfield'>
-             <label>Enter Password: </label>
-               <input
-                type='passwordone'
-                id='passwordone'
+           <Grid item>
+               <TextField
+                id='outlined-basic'
+                label='New Password'
+                variant='outlined'
+                type='password'
                 name='passwordone'
                 value={formik.values.passwordone}
                 onChange={formik.handleChange}
-                placeHolder='Enter Your Password'
                />
-           </div>
+                {formik.errors.passwordone ? <p className='errors'>{formik.errors.passwordone}</p> : null}
+           </Grid>
            
-           <div className='textfield'>
-             <label>Re-Enter Password: </label>
-               <input
-                type='passwordtwo'
-                id='passwordtwo'
+           <Grid item>
+               <TextField
+                type='password'
+                id='outlined-basic'
+                label='Re-Enter Password'
+                variant='outlined'
                 name='passwordtwo'
                 value={formik.values.passwordtwo}
                 onChange={formik.handleChange}
-                placeHolder='Re-Enter Your Password'
                />
-           </div>
-           <button type='submit'>Submit</button> <button onClick={formik.handleReset} >Reset</button>
-        </div>
+                 {formik.errors.passwordtwo ? <p className='errors'>{formik.errors.passwordtwo}</p> : null}
+           </Grid>
+           <ButtonGroup variant='contained' aria-label='outlined primary button group'>
+           <Button variant='contained' color='primary' type='submit'  >Submit</Button> 
+           <Button varinat='contained' color='primary' onClick={openNow}>view</Button>
+           <Button variant='contained' color='primary' onClick={formik.handleReset} >Reset</Button>
+           </ButtonGroup>
+          </Stack>
+          <Dialog onClose={handleCloseDialog} open={openDialog}>
+            <DialogContent>
+               <Stack>
+                 <h1>Your form submitted</h1>
+                 Your username is : {formik.values.username} <br></br>
+                 Your Email is : {formik.values.email}<br></br>
+                 Your Password is : {formik.values.passwordone}<br></br>
+               </Stack>
+            </DialogContent>
+          </Dialog>
+        </Grid>
     </form>
   );
 }
